b81384a30e47cc34ca6902e919e7ffe7d52ed85f
ngenohzephenia <ngenohzef@gmail.com>
2021-03-10 23:16:44 +0300

    initial commit

--- app.py
+++ app.py
@@ -9,5 +9,8 @@ def home():
 
 
 
+
+
+
 if __name__ =='__main__':
     app.run(debug=True)
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/Home')
 def home():
     return render_template('home.html')
 
